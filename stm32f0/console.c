587fe6ff0e7d37b09d61eddd5111cddfa878c32d
Devan Lai <devan.lai@gmail.com>
2015-11-28 13:57:47 -0800

    Replace USART2 with CONSOLE_USART where generic

--- stm32f0/console.c
+++ stm32f0/console.c
@@ -12,14 +12,14 @@ void console_setup(uint32_t baudrate) {
     gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO2 | GPIO3);
     gpio_set_af(GPIOA, GPIO_AF1, GPIO2 | GPIO3);
 
-    usart_set_baudrate(USART2, baudrate);
-    usart_set_databits(USART2, 8);
-    usart_set_parity(USART2, USART_PARITY_NONE);
-    usart_set_stopbits(USART2, USART_CR2_STOP_1_0BIT);
-    usart_set_mode(USART2, USART_MODE_TX_RX);
-    usart_set_flow_control(USART2, USART_FLOWCONTROL_NONE);
+    usart_set_baudrate(CONSOLE_USART, baudrate);
+    usart_set_databits(CONSOLE_USART, 8);
+    usart_set_parity(CONSOLE_USART, USART_PARITY_NONE);
+    usart_set_stopbits(CONSOLE_USART, USART_CR2_STOP_1_0BIT);
+    usart_set_mode(CONSOLE_USART, USART_MODE_TX_RX);
+    usart_set_flow_control(CONSOLE_USART, USART_FLOWCONTROL_NONE);
 
-    usart_enable(USART2);
+    usart_enable(CONSOLE_USART);
 }
 
 
