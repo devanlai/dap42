af617c296d15f1be36b30dcaecef624734c0ca08
Devan Lai <devan.lai@gmail.com>
2018-02-27 21:36:47 -0800

    Improve toolchain install script Cleanup broken toolchain directories

--- util/install-toolchain.sh
+++ util/install-toolchain.sh
@@ -3,10 +3,20 @@ set -eo pipefail
 URL=https://launchpad.net/gcc-arm-embedded/5.0/5-2015-q4-major/+download/gcc-arm-none-eabi-5_2-2015q4-20151219-linux.tar.bz2
 TOOLCHAIN=gcc-arm-none-eabi-5_2-2015q4
 TOOLCHAINS=$HOME/toolchains
+TOOLCHAIN_MISSING=0
+
 if [[ ! -d "${TOOLCHAINS}/gcc-arm-embedded" ]]; then
+    TOOLCHAIN_MISSING=1
+fi;
+if [[ ! -f "${TOOLCHAINS}/gcc-arm-embedded/bin/arm-none-eabi-gcc" ]]; then
+    TOOLCHAIN_MISSING=1
+fi;
+
+if [ $TOOLCHAIN_MISSING -eq 1 ]; then
     echo "Installing $TOOLCHAIN from $URL to ${TOOLCHAINS}"
     mkdir -p ${TOOLCHAINS}
     wget -qO- $URL | tar xj -C ${TOOLCHAINS}
+    rm -rf ${TOOLCHAINS}/gcc-arm-embedded
     ln -s $TOOLCHAIN ${TOOLCHAINS}/gcc-arm-embedded
 fi;
 
